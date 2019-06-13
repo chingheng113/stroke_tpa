02ec6347727fa62187110ceb5e986c472ea9e3ae
Ching-Heng <chingheng113@gmail.com>
2019-06-13 14:15:50 -0400

    back up

--- tpa.py
+++ tpa.py
@@ -7,13 +7,29 @@ current_path = os.path.dirname(__file__)
 
 
 def is_tpa():
-    df_case = pd.read_csv(os.path.join(current_path, 'data', 'raw', 'CASEDCASE.csv'))
-    df_tpa = df_case[['IVTPATH_ID', 'IVTPA_DT', 'IVTPAH_NM', 'IVTPAM_NM', 'IVTPAMG_NM', 'NIVTPA_ID']]
-    df_tpa.loc[~df_tpa.IVTPATH_ID.isin(['1', '2'])] = np.nan
+    df_case = pd.read_csv(os.path.join(current_path, 'data', 'raw', 'CASEDCASE-2016-05-2.csv'))
+    df_tpa = df_case[['ICD_ID', 'IVTPATH_ID', 'IVTPA_DT', 'IVTPAH_NM', 'IVTPAM_NM', 'IVTPAMG_NM', 'NIVTPA_ID']]
+    # ICD_ID = 1 (Infract)
+    df_tpa = df_tpa[df_tpa.ICD_ID == 1]
+    # IVTPATH_ID = 1 or 2 (1=This hospital, 2 = Another hospital)
+    df_tpa['IVTPATH_ID'].loc[~df_tpa.IVTPATH_ID.isin(['1', '2'])] = np.nan
+    # have validate date, hour, minutes ('IVTPA_DT', 'IVTPAH_NM', 'IVTPAM_NM')
     df_tpa['IVTPA_DT'] = pd.to_datetime(df_tpa['IVTPA_DT'], format='%Y-%M-%d', errors='ignore')
-    df_tpa['NIVTPA_ID'] = [np.nan if np.isnan(x) else -1 for x in df_tpa['IVTPATH_ID']]
-    df_case['is_tpa'] = [1 if x == -1 else 0 for x in df_tpa['NIVTPA_ID']]
-    df_tpa = df_case[df_case.is_tpa == 1]
+    df_tpa['IVTPAH_NM'].loc[(df_tpa.IVTPAH_NM < 0) & (df_tpa.IVTPAH_NM > 24)] = np.nan
+    df_tpa['IVTPAM_NM'].loc[(df_tpa.IVTPAM_NM < 0) & (df_tpa.IVTPAM_NM > 60)] = np.nan
+    # have IV-tPA mg
+    df_tpa['IVTPAMG_NM'].loc[(df_tpa.IVTPAMG_NM < 0) & (df_tpa.IVTPAMG_NM > 101)] = np.nan
+    # Not treat with IV t-PA (NIVTPA_ID) != 1, 2, 3
+    df_tpa['NIVTPA_ID'].loc[df_tpa.NIVTPA_ID.isna()] = -1
+    df_tpa['NIVTPA_ID'].loc[df_tpa.NIVTPA_ID > 0] = np.nan
+    df_tpa.dropna(inplace=True)
+
+
+    df_case['is_tpa'] = 0
+    df_case['is_tpa'].loc[df_tpa.index] = 1
+    print(df_case[df_case.is_tpa == 1].shape)
+
+
     return df_case
 
 
